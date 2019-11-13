a09b2cb50b0c5d4220cf3749f7ae202983c31555
Evan Shaw <evandanielshaw@gmail.com>
2019-11-13 16:53:37 +0900

    fix: make formData params merge properly with qs

--- src/createRequestBody.js
+++ src/createRequestBody.js
@@ -1,4 +1,5 @@
 import { merge } from 'lodash'
+import { stringify } from 'qs'
 
 /**
  * Creates POST body for any AJAX request
@@ -6,17 +7,26 @@ import { merge } from 'lodash'
  * This builder function expects all keys from the array returned by getScriptInjectionVariables()
  * in the PHP package 'wordpress-router' to be defined on the window object for the namespace
  * provided to this function.
- * 
+ *
  * @auther Evan D Shaw <evandanielshaw@gmail.com>
  * @see https://github.com/aivec/wordpress-router/tree/master/Router.php {function getScriptInjectionVariables()}
+ * @see https://github.com/ljharb/qs
+ * @see https://github.com/axios/axios#using-applicationx-www-form-urlencoded-format
  * @export
- * @param {object} payload
- * @param {string} route
- * @param {string} namespace window object namespace
- * @param {object} formPayload extra form fields to be merged with the final FormData object
- * @returns {FormData}
+ * @param {Object} payload
+ * @param {String} route
+ * @param {String} namespace window object namespace
+ * @param {Object} formPayload extra form fields to be merged with the final FormData object
+ * @param {Boolean} raw true: returns raw params object. false: returns qs stringified params
+ * @returns {String|Object}
  */
-export default function (payload, route, namespace, formPayload = {}) {
+export default function(
+  payload,
+  route,
+  namespace,
+  formPayload = {},
+  raw = false
+) {
   const { nonceKey, ajaxNamespace, nonce } = window[namespace]
   const params = {
     [nonceKey]: nonce,
@@ -25,8 +35,6 @@ export default function (payload, route, namespace, formPayload = {}) {
     payload: payload ? JSON.stringify(payload) : null,
   }
   merge(params, formPayload)
-  const formData = new FormData()
-  Object.keys(params).forEach((key) => formData.append(key, params[key]))
 
-  return formData
+  return raw === true ? params : stringify(params)
 }
