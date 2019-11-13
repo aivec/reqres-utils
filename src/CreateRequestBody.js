d0856481c1641b7b43729b42b7e0cf29030f233c
Evan Shaw <evandanielshaw@gmail.com>
2019-11-13 12:56:23 +0900

    feat: add formPayload param to CreateRequestBody func

--- src/CreateRequestBody.js
+++ src/CreateRequestBody.js
@@ -2,24 +2,26 @@
  * Creates POST body for any AJAX request
  *
  * This builder function expects all keys from the array returned by getScriptInjectionVariables()
- * in the PHP package 'welcart-generic' to be defined on the window object for the namespace
+ * in the PHP package 'wordpress-router' to be defined on the window object for the namespace
  * provided to this function.
  * 
  * @auther Evan D Shaw <evandanielshaw@gmail.com>
- * @see https://github.com/aivec/welcart-generic/tree/master/Controllers/Router.php {function getScriptInjectionVariables()}
+ * @see https://github.com/aivec/wordpress-router/tree/master/Router.php {function getScriptInjectionVariables()}
  * @export
  * @param {object} payload
  * @param {string} route
  * @param {string} namespace window object namespace
+ * @param {object} formPayload extra form fields to be merged with the final FormData object
  * @returns {FormData}
  */
-export default function (payload, route, namespace) {
+export default function (payload, route, namespace, formPayload = {}) {
   const { nonceKey, ajaxNamespace, nonce } = window[namespace]
   const params = {
     [nonceKey]: nonce,
     [ajaxNamespace]: 1,
     route,
     payload: payload ? JSON.stringify(payload) : null,
+    ...formPayload,
   }
   const formData = new FormData()
   Object.keys(params).forEach((key) => formData.append(key, params[key]))
